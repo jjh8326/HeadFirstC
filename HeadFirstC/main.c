4499e7e0e0b3d2f5053be9d1b6ced6b91a1545c8
Joe Harasz <j30h30@github.com>
2019-11-13 23:13:35 -0500

    Initial Commit

---
+++ HeadFirstC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HeadFirstC
+//
+//  Created by Joe Harasz on 11/13/19.
+//  Copyright © 2019 JJH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
